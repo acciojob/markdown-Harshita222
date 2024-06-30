3c37700b145cb466526d6b426dfec1c0b1435067
Harshita222 <kumariharsita501@gmail.com>
2024-06-30 15:46:37 +0530

    do

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Mark1 from './Mark1'
-import Mark2 from './Mark2'
+import Mark1 from '../components/Mark1'
+import Mark2 from '../components/Mark2'
 let App= ()=>{
     return (
         <div className='app'>
-          
+
             <Mark1/>
         </div>
     )
