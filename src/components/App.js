5174e7874d3cc6665e5dc26284844a3a55a8bc8d
Harshita222 <kumariharsita501@gmail.com>
2024-07-01 10:58:59 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Mark1 from '../components/Mark1';
-import '../styles/App.css';
+import React from 'react'
+import Mark1 from '../components/Mark1'
+import Mark2 from '../components/Mark2'
+import '../styles/App.css'
+let App= ()=>{
+    return (
+        <div className='app'>
 
-const App = () => {
-  return (
-    <div className="app">
-      <Mark1 />
-    </div>
-  );
-};
-
-export default App;
+            <Mark1/>
+        </div>
+    )
+}
+export default App
+a
